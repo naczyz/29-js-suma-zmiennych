443f5abe78a58d83425a832e1eb5a88eca522875
naczyz <natan.czyz@gmail.com>
2023-08-09 12:03:02 +0200

    dodanie zadania js suma

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let firstNumber = 10;
+let secoundNumber = 20;
+
+function multiply(firstNumber, secoundNumber) {
+  let result = firstNumber + secoundNumber;
+  return result;
+}
+console.log(multiply(10, 20));
